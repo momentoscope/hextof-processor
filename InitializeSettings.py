c29d68e89f27919da045a8bd8dbad275f159534f
Steinn Y. Agustsson <sagustss@uni-mainz.de>
2018-03-28 22:24:49 +0200

    improved settings managment

--- InitializeSettings.py
+++ InitializeSettings.py
@@ -6,9 +6,6 @@ import psutil
 
 """Create a SETTINGS.ini file
 
-WARNING:
-    Running this script overwrites any previously existing Settings.
-
 This script creates or overwrites a preexisting SETTINGS.ini file.
 Such file contains local user information such as absolute paths for raw data, processed parquet dataframes and local 
 machine settings, such as number of cores and conversion quantities.
@@ -19,39 +16,54 @@ directly on the SETTINGS.ini file.
 This was created to allow this code to be machine and OS agnostic.
 """
 
+
+def make_new_settings():
+    config = configparser.ConfigParser()
+
+    settings_dict = {
+        'paths': {
+            'DATA_RAW_DIR': 'D:/data/FLASH/_RAW/online-3/',
+            'DATA_H5_DIR': 'D:/data/FLASH/_processed/h5',
+            'DATA_PARQUET_DIR': 'D:/data/FLASH/_processed/parquet',
+            'DATA_RESULTS_DIR': 'D:/data/FLASH/Results',
+            'PAH_MODULE_DIR': 'D:/py_code/FLASH/PAH/',
+        },
+        'processor': {
+            'N_CORES': psutil.cpu_count(),
+            'CHUNK_SIZE': 1000000,
+            'ToF_STEP_NS': 0.0205761316872428,
+            'ToF_STEP_EV': 1,
+        }
+    }
+
+    # write dictionary to .ini structure
+    for section_name, section in settings_dict.items():
+        config.add_section(section_name)
+        for key, val in section.items():
+            config.set(section_name, key, str(val))
+
+    missing_folders = []
+    for dir_name in config['paths']:
+        if not os.path.isdir(config['paths'][dir_name]):
+            missing_folders.append((dir_name, config['paths'][dir_name]))
+    if len(missing_folders) > 0:
+        print('WARNING: the following folders do not exist:')
+        for i in missing_folders:
+            print('{0}: {1}'.format(i[0], i[1]))
+
+    # write .ini structure to file.
+    with open('SETTINGS.ini', 'w') as configfile:  # save
+        config.write(configfile)
+    print(' -> New setting file generated.')
+
+
 if os.path.isfile('SETTINGS.ini'):
-    print(' -> Overwriting previous settings.')
+    answer = input('Overwriting existing settings.\nAre you sure? [y/n]')
+    if answer.lower() in ['y', 'yes', 'si', 'ja']:
+        print(' -> Overwriting previous settings.')
+        make_new_settings()
+    else:
+        print('Settings file generation aborted by user.')
 else:
     print(' -> No Settings found. Initializing new Settings file.')
-
-config = configparser.ConfigParser()
-
-entries = {
-    'paths': {
-        'FLASH_RAW_DATA_PATH': 'D:/data/FLASH/_RAW/online-3/',
-        'PAH_MODULE_PATH': 'D:/py_code/FLASH/PAH/',
-        'DATA_H5_PATH': 'D:/data/FLASH/_processed/h5',
-        'DATA_PARQUET_PATH': 'D:/data/FLASH/_processed/parquet',
-    },
-    'processor': {
-        'N_CORES': psutil.cpu_count(),
-        'CHUNK_SIZE': 1000000,
-        'MAX_INTERVAL_SIZE': 30000,
-    },
-    'parameters': {
-        'ToF_TIME_STEP': 0.0205761316872428,
-        'ToF_energy_step': 1,
-
-    }
-}
-# write dictionary to .ini structure
-for section_name, section in entries.items():
-    config.add_section(section_name)
-    for key, val in section.items():
-        config.set(section_name, key, str(val))
-
-# write .ini structure to file.
-with open('SETTINGS.ini', 'w') as configfile:  # save
-    config.write(configfile)
-
-print(' -> Setting file generated.')
+    make_new_settings()
