ec461a69dd4b3e1a869bc6621541e6c8720c5116
Davide Curcio <davide.curcio@gmail.com>
2018-04-16 19:33:24 +0200

    Slight change to setup.py

--- setup.py
+++ setup.py
@@ -9,15 +9,9 @@ from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
 import os
-import platform
-if platform.system() == 'Windows':
-    setup(
-        ext_modules=cythonize("processor\cscripts\DldFlashProcessorCy.pyx"),
-        include_dirs = [numpy.get_include()]
-    )
-else:
-    setup(
-        ext_modules=cythonize("processor/cscripts/DldFlashProcessorCy.pyx"),
-        include_dirs = [numpy.get_include()]
-    )
+
+setup(
+    ext_modules=cythonize(os.path.join("processor","cscripts","DldFlashProcessorCy.pyx")),
+    include_dirs=[numpy.get_include()]
+)
 print('Cythonized: DldFlashProcessorCy.pyx')
