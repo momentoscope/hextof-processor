166c174b42ad9c8b8760d6c0598de00a06e6a9ed
Steinn Ymir Agustsson <sagustss@uni-mainz.de>
2018-04-16 19:16:57 +0200

    made system independent

--- setup.py
+++ setup.py
@@ -8,9 +8,16 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
-
-setup(
-    ext_modules=cythonize("processor\cscripts\DldFlashProcessorCy.pyx"),
-    include_dirs = [numpy.get_include()]
-)
+import os
+import platform
+if platform.system() == 'Windows':
+    setup(
+        ext_modules=cythonize("processor\cscripts\DldFlashProcessorCy.pyx"),
+        include_dirs = [numpy.get_include()]
+    )
+else:
+    setup(
+        ext_modules=cythonize("processor/cscripts/DldFlashProcessorCy.pyx"),
+        include_dirs = [numpy.get_include()]
+    )
 print('Cythonized: DldFlashProcessorCy.pyx')
