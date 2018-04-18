e5bb8aa1e7d9ceb06b912ee3b54b9ef07012f282
Steinn Ymir Agustsson <sagustss@uni-mainz.de>
2018-04-18 10:59:49 +0200

    uniformed docstring style

--- processor/utils.py
+++ processor/utils.py
@@ -132,6 +132,35 @@ Parameters:
 
 # TODO: include offs and oo in the SETTINGS.ini file
 def t2e(t):
+    """ Transform ToF to eV.
+
+    The functions (t2e and e2t) convert between binding energy (Eb) in eV (negative convention)
+    and time of flight (ToF) in ns.
+    The formula used is based on the ToF for an electron with a kinetic energy Ek. Then the
+    binding energy Eb is given by
+    Eb = Ek+W-hv-V = 1/2 m*v*v +W-hv-V
+    With W the work function, hv the photon energy, V the electrostatic potential applied to
+    the sample, v the velocity of the electrons in the drift tube, m the mass of the electron.
+    The velocity v in the drift tube can be calculated knowing the length (1m) and the flight
+    time in the drift tube. The measured ToF, however, has some offset due to clock start not
+    coinciding with entry in the drift section.
+
+    offs is supposed to include the time offset for when the electrons enter the drift section.
+    Its main mainly affects peak spacing, and there are several strategies for calibrating this
+    value:
+        1.  By measuring the photon peak and correcting by some extractor voltage-dependent offset
+        2.  Shifting the potential by 1V and imposing the same shift in the measured spectrum
+        3.  Imposing some calibrated spacing between features in a spectrum
+
+    oo is supposed to include -W+hv+V. It mainly affects absolute position of the peaks, and
+    there are several strategies for calibrating this value:
+        1.  By getting the correct values for W, hv, and V
+        2.  It can be calibrated by imposing peak position
+
+    Parameters:
+        t (float) the ToF
+        e (float) the binding energy
+    """
     offs = 371.258
     oo = 323.98
 
@@ -140,6 +169,35 @@ def t2e(t):
 
 
 def e2t(e):
+    """ Transform eV to ToF.
+
+    The functions (t2e and e2t) convert between binding energy (Eb) in eV (negative convention)
+    and time of flight (ToF) in ns.
+    The formula used is based on the ToF for an electron with a kinetic energy Ek. Then the
+    binding energy Eb is given by
+    Eb = Ek+W-hv-V = 1/2 m*v*v +W-hv-V
+    With W the work function, hv the photon energy, V the electrostatic potential applied to
+    the sample, v the velocity of the electrons in the drift tube, m the mass of the electron.
+    The velocity v in the drift tube can be calculated knowing the length (1m) and the flight
+    time in the drift tube. The measured ToF, however, has some offset due to clock start not
+    coinciding with entry in the drift section.
+
+    offs is supposed to include the time offset for when the electrons enter the drift section.
+    Its main mainly affects peak spacing, and there are several strategies for calibrating this
+    value:
+        1.  By measuring the photon peak and correcting by some extractor voltage-dependent offset
+        2.  Shifting the potential by 1V and imposing the same shift in the measured spectrum
+        3.  Imposing some calibrated spacing between features in a spectrum
+
+    oo is supposed to include -W+hv+V. It mainly affects absolute position of the peaks, and
+    there are several strategies for calibrating this value:
+        1.  By getting the correct values for W, hv, and V
+        2.  It can be calibrated by imposing peak position
+
+    Parameters:
+        t (float) the ToF
+        e (float) the binding energy
+    """
     offs = 371.258
     oo = 323.98
 
