5be55d944ed5eefc73f08a691d2f2bcfa4358738
Davide Curcio <davide.curcio@gmail.com>
2018-04-23 16:09:07 +0200

    Auto stash before merge of "master" and "origin/master"

--- setup.py
+++ setup.py
@@ -4,12 +4,19 @@
 @author: Steinn Ymir Agustsson
 """
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
 import numpy
 import os
 
+
+extensions = [
+    Extension("processor.cscripts.DldFlashProcessorCy", [os.path.join("processor", "cscripts", "DldFlashProcessorCy.pyx")],
+        include_dirs=[numpy.get_include()])
+]
+
 setup(
-    ext_modules=cythonize(os.path.join("processor", "cscripts", "DldFlashProcessorCy.pyx")),
-    include_dirs=[numpy.get_include()]
+    name="DldFlashProcessorCy",
+    ext_modules=cythonize(extensions)
 )
 print('Cythonized: DldFlashProcessorCy.pyx')
