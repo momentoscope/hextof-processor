3f7b27e779564d1d4a24ce368107b04debf0c5f6
Davide Curcio <davide.curcio@gmail.com>
2018-04-23 17:44:17 +0200

    Add support for dsgn functions

--- setupDsgn.py
+++ setupDsgn.py
@@ -9,14 +9,11 @@ import sys
 import os
 
 XPSdoniachs = Extension('pkg.XPSdoniachs_ext',
-                        sources=['pythonFitting/XPSdoniachs_ext/XPSdoniachs_ext.cpp'],
-                        include_dirs=['/usr/local/include'],
-                        library_dirs=['/usr/local/lib/boost'],
-                        runtime_library_dirs=['/usr/local/lib/boost'],
-                        libraries=['boost_python'])
+                        sources=[os.path.join('pythonFitting','XPSdoniachs_ext','XPSdoniachs_ext.cpp')],
+                        libraries=['boost_system'])
 
 setup(name='XPSdoniachs',
       version='0.1',
       description='XPSdoniachs',
-      packages=['pkg','boost'],
+      packages=['pkg'],
       ext_modules=[XPSdoniachs])
