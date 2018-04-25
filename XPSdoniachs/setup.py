12a1169f0da01754cc2957123cd4bccf9b150053
Davide Curcio <davide.curcio@gmail.com>
2018-04-25 03:26:04 +0200

    Clean up of cython builds

--- XPSdoniachs/setup.py
+++ XPSdoniachs/setup.py
@@ -6,11 +6,11 @@
 from distutils.core import setup, Extension
 import os
 
-XPSdoniachs = Extension('pkg.XPSdoniachs_ext',
-                        sources=[os.path.join('pythonFitting','XPSdoniachs_ext','XPSdoniachs_ext.cpp')],
+XPSdoniachs = Extension('XPSdoniachs_ext',
+                        sources=['XPSdoniachs_ext.cpp'],
                         library_dirs=["/usr/local/Cellar/boost-python3/1.67.0/lib/"],
                         libraries=["boost_python36"])
 
 setup(name='XPSdoniachs',
-      packages=['pkg'],
+      packages=['.'],
       ext_modules=[XPSdoniachs])
