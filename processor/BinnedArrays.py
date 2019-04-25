42a9a4f961b32d00532f96aad67aed8c1c17b79c
Soeren Grunewald <pg2user@hasfcons1.desy.de>
2019-04-26 01:03:11 +0200

    Add new BinnedArrays class for binned data

---
+++ processor/BinnedArrays.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+
+import os
+from configparser import ConfigParser
+import xarray as xr
+import utilities.misc as utils
+import h5py
+import numpy as np
+
+class BinnedArray(xr.DataArray):
+    
+    def __init__(self,data=None, coords=None, dims=None, name=None,attrs=None, encoding=None, indexes=None, fastpath=False):
+        super(BinnedArray,self).__init__(data, coords=coords, dims=dims, name=name,
+            attrs=attrs, encoding=encoding, indexes=indexes, fastpath=fastpath)
+
+    def to_h5(self, file_name, path=None, mode='w'):
+        """ Save a binned numpy array to h5 file. The file includes the axes
+        (taken from the scheduled bins) and the delay stage histogram, if it exists.
+
+        :Parameters:
+            file_name : str
+                Name of the saved file. The extension '.h5' is automatically added.
+            path : str | None
+                File path.
+            mode : str | 'w'
+                Write mode of h5 file ('w' = write).
+        """
+        _abort = False
+
+        if path is None:
+            path = utils.parse_setting('paths','DATA_H5_DIR')
+        if not os.path.isdir(path):  # test if the path exists...
+            answer = input("The folder {} doesn't exist,"
+                           "do you want to create it? [y/n]".format(path))
+            if 'y' in answer:
+                os.makedirs(path)
+            else:
+                _abort = True
+
+        filename = '{}.h5'.format(file_name)
+        if os.path.isfile(path + filename):
+            answer = input("A file named {} already exists. Overwrite it? "
+                           "[y/n or r for rename]".format(filename))
+            if answer.lower() in ['y', 'yes', 'ja']:
+                pass
+            elif answer.lower() in ['n', 'no', 'nein']:
+                _abort = True
+            else:
+                filename = input("choose a new name:")
+
+        if not _abort:
+            with h5py.File(path + filename, mode) as h5File:
+
+                print('saving data to {}'.format(path + filename))
+
+                if 'pumpProbeTime' in self.dims:
+                    idx = self.dims.index('pumpProbeTime')
+                    pp_data = np.swapaxes(self.data, 0, idx)
+
+                elif 'delayStage' in self.dims:
+                    idx = self.dims.index('delayStage')
+                    pp_data = np.swapaxes(self.data, 0, idx)
+                else:
+                    pp_data = None
+
+                # Saving data
+
+                ff = h5File.create_group('frames')
+
+                if pp_data is None:  # in case there is no time axis, make a single dataset
+                    ff.create_dataset('f{:04d}'.format(0), data=self.data)
+                else:
+                    for i in range(pp_data.shape[0]):  # otherwise make a dataset for each time frame.
+                        ff.create_dataset('f{:04d}'.format(i), data=pp_data[i, ...])
+
+                # Saving axes
+                aa = h5File.create_group("axes")
+                # aa.create_dataset('axis_order', data=self.dims)
+                ax_n = 1
+                for binName in self.dims:
+                    if binName in ['pumpProbeTime', 'delayStage']:
+                        ds = aa.create_dataset('ax0 - {}'.format(binName), data=self.coords[binName])
+                        ds.attrs['name'] = binName
+                    else:
+                        ds = aa.create_dataset('ax{} - {}'.format(ax_n, binName), data=self.coords[binName])
+                        ds.attrs['name'] = binName
+                        ax_n += 1
+                # Saving delay histograms
+                hh = h5File.create_group("histograms")
+                if hasattr(self, 'delaystageHistogram'):
+                    hh.create_dataset(
+                        'delaystageHistogram',
+                        data=self.delaystageHistogram)
+                if hasattr(self, 'pumpProbeHistogram'):
+                    hh.create_dataset(
+                        'pumpProbeHistogram',
+                        data=self.pumpProbeHistogram)
+
+
+    def read_h5(self,filename):
+        raise NotImplementedError()
+
