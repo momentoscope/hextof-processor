b1fc7721f1ec954a7f4ec0cad211c2bc7680842b
RealPolitiX <xrpatrick@gmail.com>
2020-11-24 03:21:54 -0600

    Added Michael Heber as coauthor

--- processor/__init__.py
+++ processor/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from . import utilities
+
 __version__ = '0.9.7'
-__author__ = 'Yves Acremann, Steinn Ymir Agustsson, Davide Curcio, Rui Patrick Xian, Maciej Dendzik'
+__author__ = 'Yves Acremann, Steinn Ymir Agustsson, Davide Curcio, Rui Patrick Xian, Michael Heber, Maciej Dendzik'
