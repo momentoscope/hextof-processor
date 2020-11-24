0dd24699b2518285434b58d8a5550cc9eb8deff7
RealPolitiX <xrpatrick@gmail.com>
2020-11-24 03:30:18 -0600

    Update version to 1.0.1

--- processor/__init__.py
+++ processor/__init__.py
@@ -2,5 +2,5 @@
 
 from . import utilities
 
-__version__ = '0.9.7'
+__version__ = '1.0.1'
 __author__ = 'Yves Acremann, Steinn Ymir Agustsson, Davide Curcio, Rui Patrick Xian, Michael Heber, Maciej Dendzik'
