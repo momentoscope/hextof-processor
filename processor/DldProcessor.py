670547ba52f121264a164e753167850796941fd8
Unknown <davide.curcio@gmail.com>
2020-11-28 08:32:44 +0100

    change energycalibration to be consistent with binding energy

--- processor/DldProcessor.py
+++ processor/DldProcessor.py
@@ -569,14 +569,14 @@ class DldProcessor:
                                                                   col='dldTime_corrected', type=jitterType)
 
         if useAvgSampleBias:
-            eoffset -= self.dd['sampleBias'].mean()
+            eoffset += self.dd['sampleBias'].mean()
         else:
-            eoffset -= self.dd['sampleBias']
+            eoffset += self.dd['sampleBias']
 
         if useAvgMonochormatorEnergy:        # TODO: add monocrhomator position,
-            eoffset -= self.dd['monochromatorPhotonEnergy'].mean()
+            eoffset += self.dd['monochromatorPhotonEnergy'].mean()
         else:
-            eoffset -= self.dd['monochromatorPhotonEnergy']
+            eoffset += self.dd['monochromatorPhotonEnergy']
 
         if useAvgSampleBias or useAvgMonochormatorEnergy:
             print('computing energy offsets...')
