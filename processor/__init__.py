572537f287589bf36cead37736f898db0caa2bf8
Steinn Ymir Agustsson <sagustss@uni-mainz.de>
2021-01-07 23:17:45 +0100

    Fixed import bug

--- processor/__init__.py
+++ processor/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from . import utilities
+from . import utilities, DldProcessor
 
 __version__ = '1.0.3'
 __author__ = 'Yves Acremann, Steinn Ymir Agustsson, Davide Curcio, Rui Patrick Xian, Michael Heber, Maciej Dendzik'
